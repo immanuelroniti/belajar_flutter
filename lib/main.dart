f3440a31a6fcf1eb0355449a12b2e97d6e53058c
Roni <immanuel.roni@ti.ukdw.ac.id>
2018-12-28 12:37:42 +0700

    styling with icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(new MaterialApp(
     home: new HalamanSatu(),
   ));
 }
 
-class HalamanSatu extends StatelessWidget{
-
+class HalamanSatu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new  Container(
-        color: Colors.lightBlueAccent,
-        child: new Center(
-          child: new Text("Belajar Styling"),
+      body: new Center(
+        child: new Container(
+          color: Colors.pink,
+          width: 50.0,
+          height: 50.0,
+          child: new Center(
+            child: new Icon(
+              Icons.favorite,
+              color: Colors.white,
+            ),
+          ),
         ),
       ),
     );
