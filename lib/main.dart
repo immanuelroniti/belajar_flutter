2e3c3fe77d33119794c5cb2b20541c8f6837291a
Roni <immanuel.roni@ti.ukdw.ac.id>
2018-12-28 12:48:36 +0700

    add appBar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(new MaterialApp(
+    title: "aplikasi buatan roni",
     home: new HalamanSatu(),
   ));
 }
@@ -9,19 +10,16 @@ void main() {
 class HalamanSatu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Center(
-        child: new Container(
-          color: Colors.pink,
-          width: 50.0,
-          height: 50.0,
-          child: new Center(
-            child: new Icon(
-              Icons.favorite,
-              color: Colors.white,
-            ),
-          ),
+    return new Scaffold( // seperti activity
+      appBar: new AppBar(
+        backgroundColor: Colors.pink,
+        leading: new Icon(Icons.home),
+        title: new Center(
+          child: new Text("roni app"),
         ),
+        actions: <Widget>[
+          new Icon(Icons.search)
+        ],
       ),
     );
   }
