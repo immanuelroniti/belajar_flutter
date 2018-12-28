bef6328d44e65aba74642394d39809373097b24b
Roni <immanuel.roni@ti.ukdw.ac.id>
2018-12-28 13:04:43 +0700

    Row and Columns

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,32 @@ void main() {
 class HalamanSatu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold( // seperti activity
+    return new Scaffold(
+      // seperti activity
       appBar: new AppBar(
         backgroundColor: Colors.pink,
         leading: new Icon(Icons.home),
         title: new Center(
           child: new Text("roni app"),
         ),
-        actions: <Widget>[
-          new Icon(Icons.search)
-        ],
+        actions: <Widget>[new Icon(Icons.search)],
+      ),
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            new Icon(Icons.favorite, size: 70.0, color: Colors.green),
+            new Icon(Icons.favorite, size: 70.0, color: Colors.green),
+            new Row(
+              children: <Widget>[
+                new Icon(Icons.favorite, size: 70.0, color: Colors.blue),
+                new Icon(Icons.favorite, size: 70.0, color: Colors.blue),
+                new Icon(Icons.favorite, size: 70.0, color: Colors.blue),
+                new Icon(Icons.favorite, size: 70.0, color: Colors.blue),
+              ],
+            ),
+            new Icon(Icons.favorite, size: 70.0, color: Colors.pink),
+          ],
+        ),
       ),
     );
   }
